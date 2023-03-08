89ef557961f4aa68d86218dbe4d15e2af941aa92
any <any@user.tax>
2023-03-09 00:24:11 +0800

    ♨

--- chasquid/init.sh
+++ chasquid/init.sh
@@ -57,15 +57,40 @@ if [ ! -f "$private" ]; then
 fi
 
 link() {
-  if [ ! -e "$1" ]; then
-    ln -s /mnt/www/.acme.sh/${HOST}_ecc/$1 ${2:-"$1"}
+  fp=${2:-"$1"}
+  if [ ! -e "$fp" ]; then
+    ln -s /mnt/www/.acme.sh/${HOST}_ecc/$1 $fp
   fi
 }
 link fullchain.cer
 link $HOST.key privkey.pem
+cd ../..
+d=domains/$HOST
+mkdir -p $d
+cd $d
 
-cd $DIR
+if [ ! -f "aliases" ]; then
+  echo -e "i: i.$HOST@gmail.com\n*: i.$HOST@gmail.com" >aliases
+fi
+
+if [ ! -f "dkim_selector" ]; then
+  dkim=$(node -e "console.log(($(($(date +%s) / 86400))).toString(36))")
+  echo $dkim >dkim_selector
+else
+  dkim=$(cat dkim_selector)
+fi
+
 chown -R chasquid $conf
 
+rsync --ignore-existing -av $DIR/conf/ $conf
 systemctl enable chasquid --now
 systemctl restart chasquid
+
+set +x
+green() {
+  echo -e "\033[32m$1\033[0m"
+}
+echo -e "Setting up DKIM → ADD DNS TXT : $(green $dkim._domainkey.$HOST)"
+
+cat $cert/dns.txt
+echo ''
