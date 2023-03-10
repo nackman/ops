28e11369c5e53b15fdbada6534392b4df62967bd
any <any@user.tax>
2023-03-11 00:00:54 +0800

    ♨

--- ssl/ali/setup.sh
+++ ssl/ali/setup.sh
@@ -8,9 +8,7 @@ set -ex
 
 exe=lib/ssl.js
 
-job="0 0 * * * eval \$(rtx env) && cd $DIR && timeout 1h $exe > /var/log/crontab.ssl.log 2>&1"
-
-# job="0 2 */10 * * timeout 1h $exe > /var/log/crontab.ssl.log 2>&1"
+job="0 3 */10 * * eval \$(rtx env) && cd $DIR && timeout 1h $exe > /var/log/crontab.ssl.log 2>&1"
 
 crontab -l | (
   cat | grep -v -F $DIR
