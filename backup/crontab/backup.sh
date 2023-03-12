33c3ae2ae97a0b61eff0be21c61489c6ab0c4b4d
any <any@user.tax>
2023-03-12 13:43:46 +0800

    ♨

--- backup/crontab/backup.sh
+++ backup/crontab/backup.sh
@@ -4,11 +4,7 @@ DIR=$(dirname $(realpath "$0"))
 cd $DIR
 set -ex
 
-job="$((59 * RANDOM / 32768)) $((24 * RANDOM / 32768)) * * * $DIR/crontab/backup.sh"
+BACKUP=/mnt/backup/crontab
+mkdir -p $BACKUP
 
-crontab -l | (
-  cat | grep -v -F $DIR
-  echo "$job"
-) | crontab -
-
-crontab -l | cat
+crontab -l >$BACKUP/$(hostname).txt
--- backup/crontab/setup.sh
+++ backup/crontab/setup.sh
@@ -4,7 +4,11 @@ DIR=$(dirname $(realpath "$0"))
 cd $DIR
 set -ex
 
-job="$((59 * RANDOM / 32768)) $((24 * RANDOM / 32768)) * * * $DIR/crontab/backup.sh"
+job="$((59 * RANDOM / 32768)) $((24 * RANDOM / 32768)) * * * $DIR/backup.sh"
+
+if [ ! -d "/mnt/backup" ]; then
+  git clone git@github.com:user-tax-key/backup.git /mnt/backup
+fi
 
 crontab -l | (
   cat | grep -v -F $DIR
